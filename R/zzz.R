631126b04336d5bf470eef080e5800d3f7e7a029
César-Lobato <ceslobfer95@gmail.com>
2023-04-19 16:23:57 +0200

    Fix errors library torch

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,37 @@
 
+# installedTORCH <- requireNamespace("torch", quietly = T)
+#
+# installedTENSORFLOW <- requireNamespace("tensorflow", quietly = T)
 
+# functionTorchInstalled<- function(){
+#   if(requireNamespace('torch') & installedTORCH){
+#     return(torch::torch_is_installed())
+#   }
+# }
+# installTorch<- functionTorchInstalled()
+installTorch<-F
 .onAttach <- function (libname, pkgname) {
 
   installedTORCH <- requireNamespace("torch", quietly = T)
   installedTENSORFLOW <- requireNamespace("tensorflow", quietly = T)
-  if (installedTORCH) attachNamespace("torch")
+  # print(installedTORCH)
+  # if (installedTORCH){
+  #   tryCatch(attachNamespace("torch"))
+    # installTorch <- torch::torch_is_installed()
+    # print(installTorch)
+    # if (installTorch) {
+    #   attachNamespace("torch")
+    # }
+
+  # if (installedTORCH){
+  #
+  #   installTorch <- torch::torch_is_installed()
+  #   if(installTorch) attachNamespace("torch")
+  # }
   if (installedTENSORFLOW)attachNamespace("tensorflow")
 
-  if (installedTORCH) packageStartupMessage("Torch tensors allowed")
+
+  if(installedTORCH & installTorch) packageStartupMessage("Torch tensors allowed")
   if (installedTENSORFLOW) packageStartupMessage("Tensorflow tensors allowed")
   if(!installedTORCH & !installedTENSORFLOW) packageStartupMessage("Not torch or tensorflow installed")
 
