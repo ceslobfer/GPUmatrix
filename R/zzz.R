50f2cac887a429f138db3d47d7e0b2faecdffbe5
César-Lobato <ceslobfer95@gmail.com>
2023-04-20 10:41:50 +0200

    installTorch

--- R/zzz.R
+++ R/zzz.R
@@ -1,39 +1,23 @@
 
-# installedTORCH <- requireNamespace("torch", quietly = T)
-#
-# installedTENSORFLOW <- requireNamespace("tensorflow", quietly = T)
+installTorch <- tryCatch({
+  res <- FALSE
+  installedTORCH <- requireNamespace("torch", quietly = T)
+  if (installedTORCH){
+    res <- torch::torch_is_installed()
+    if(res) attachNamespace("torch")
+  }
+  res
+})
 
-# functionTorchInstalled<- function(){
-#   if(requireNamespace('torch') & installedTORCH){
-#     return(torch::torch_is_installed())
-#   }
-# }
-# installTorch<- functionTorchInstalled()
-installTorch<-F
 .onAttach <- function (libname, pkgname) {
 
-  installedTORCH <- requireNamespace("torch", quietly = T)
   installedTENSORFLOW <- requireNamespace("tensorflow", quietly = T)
-  # print(installedTORCH)
-  # if (installedTORCH){
-  #   tryCatch(attachNamespace("torch"))
-    # installTorch <- torch::torch_is_installed()
-    # print(installTorch)
-    # if (installTorch) {
-    #   attachNamespace("torch")
-    # }
 
-  # if (installedTORCH){
-  #
-  #   installTorch <- torch::torch_is_installed()
-  #   if(installTorch) attachNamespace("torch")
-  # }
   if (installedTENSORFLOW)attachNamespace("tensorflow")
 
-
-  if(installedTORCH & installTorch) packageStartupMessage("Torch tensors allowed")
+  if(installTorch) packageStartupMessage("Torch tensors allowed")
   if (installedTENSORFLOW) packageStartupMessage("Tensorflow tensors allowed")
-  if(!installedTORCH & !installedTENSORFLOW) packageStartupMessage("Not torch or tensorflow installed")
+  if(installTorch==F & installedTENSORFLOW==F) packageStartupMessage("Not torch or tensorflow installed")
 
 }
 
